8623fb0f44c5a636760c7f1758a4340127aea3ba
sachin.lubana <sachin.lubana@b2x.com>
2017-11-12 22:23:08 +0530

    moved redirect to posts instead of home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-	{path: '', redirectTo: '/home', pathMatch: 'full'},
+	{path: '', redirectTo: '/posts', pathMatch: 'full'},
 	{path: 'home', component: HomeComponent},
 	{path: 'posts', component: PostsComponent},
 	{path: '**', component: PageNotFoundComponent}
